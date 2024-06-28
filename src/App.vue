aef287b4f32e4521fbac20b843fe606f39c116e7
jungle75 <jungle7503@gmail.com>
2024-06-28 10:03:13 +0900

    추가

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="introWrap">
+    <div class="introTitle">
+      <span class="logo"><img src="@/assets/images/common/logo.svg" alt="프론트박스"></span>
+      <h1>프론트박스</h1>
+      <p>당신의 아이디어를 현실로 구현하는 퍼블리싱<br>품질과 혁신의 퍼블리싱 솔루션</p>
+    </div>    
+
+    <div class="intLink">
+      <div class="itemBox">
+        <i class="pi pi-id-card"></i>
+        <h2 class="profile">Profile</h2>
+        <BaseButton label="Go" size="small" icon="pi pi-arrow-right" />
+      </div>
+      <div class="itemBox">
+        <i class="pi pi-briefcase"></i>
+        <h2 class="career">Career</h2>
+        <BaseButton label="Go" size="small" icon="pi pi-arrow-right" />
+      </div>
+      <div class="itemBox">
+        <i class="pi pi-user"></i>
+        <h2 class="portfolio">Portfolio</h2>
+        <BaseButton label="Go" size="small" icon="pi pi-arrow-right" />
+      </div>
+    </div>
+
+    <div class="footer">Copyright © FrontBox Co.</div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import '@/assets/css/style.css';
+import "primeicons/primeicons.css";
+
+import Button from 'primevue/button';
+import Card from 'primevue/card';
+
+// eslint-disable-next-line
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+app.component('BaseButton', Button);
+app.component('CardBox', Card);
+
+app.mount('#app')
